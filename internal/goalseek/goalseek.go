79fcf6d861e232da924b710b1f36783e904a59e7
Sumod Madhavan <ubuntu@PF448X51>
2024-08-26 17:50:38 +0530

    goal seek changes

--- internal/goalseek/goalseek.go
+++ internal/goalseek/goalseek.go
@@ -2,8 +2,40 @@ package goalseek
 
 import (
 	"financialapi/internal/financials"
+	"fmt"
 )
 
+type GoalSeek struct {
+	Params financials.FinancialParams
+}
+
+func (gs *GoalSeek) CalculateCumulativeProfit() (float64, error) {
+	optimalRate, _, err := financials.GoalSeek(gs.Params.TargetProfit, gs.Params, gs.Params.InitialRate)
+	if err != nil {
+		return 0, err
+	}
+
+	return financials.CalculateFinancials(optimalRate, gs.Params)
+}
+
+func (gs *GoalSeek) GetParams() interface{} {
+	return gs.Params
+}
+
+func (gs *GoalSeek) SetParams(params interface{}) error {
+	if p, ok := params.(financials.FinancialParams); ok {
+		gs.Params = p
+		return nil
+	}
+	return fmt.Errorf("invalid params type for GoalSeek")
+}
+
+// NewGoalSeekCalculator creates a new GoalSeek instance
+func NewGoalSeekCalculator(params financials.FinancialParams) financials.FinancialCalculator {
+	return &GoalSeek{Params: params}
+}
+
+// Calculate function remains the same, but now uses financials.FinancialParams
 func Calculate(params financials.FinancialParams) (map[string]interface{}, error) {
 	optimalRate, iterations, err := financials.GoalSeek(params.TargetProfit, params, params.InitialRate)
 	if err != nil {
